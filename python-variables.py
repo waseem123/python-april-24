cb9509bb030580ded5ffb70d8050854a9de4b449
WASEEM ATTAR <waseem_6957@yahoo.com>
2024-04-22 20:21:26 +0530

    Add files via upload

---
+++ python-variables.py
@@ -0,0 +1,18 @@
+a = 20
+b = 60
+c1a = 80
+_firstname_="Waseem"
+x = 200
+X = 60
+firstnum = 1000
+secondnum = 200
+print(a)
+print(b)
+print(c1a)
+print(_firstname_)
+print(x)
+print(X)
+addition = firstnum + secondnum
+print(f"ADDITION OF {firstnum} and {secondnum} IS {firstnum + secondnum}")
+result = addition * 2
+print(result)
